a796521a80c47191a3df60d3118626ada35b2585
Eduardo Neves <neves98@outlook.com>
2020-10-14 06:30:09 -0300

    master: rename columns

--- ml_treino_url.py
+++ ml_treino_url.py
@@ -5,8 +5,21 @@ import pandas as pd
 uri = "https://gist.githubusercontent.com/guilhermesilveira/2d2efa37d66b6c84a722ea627a897ced/raw/10968b997d885cbded1c92938c7a9912ba41c615/tracking.csv"
 dados = pd.read_csv(uri)
 
-x = dados[["home", "how_it_works", "contact"]]
-y = dados["bought"]
+# Rename columns
+mapa = {
+    "home" : "principal",
+    "how_it_works" : "como_funciona",
+    "contact" : "contato",
+    "bought" : "comprou"
+}
+dados = dados.rename(columns = mapa)
+
+x = dados[["principal","como_funciona","contato"]]
+x.head()
+
+y = dados["comprou"]
+y.head()
+
 
 treino_x = x[:75]
 treino_y = y[:75]
